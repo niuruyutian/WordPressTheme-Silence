eff0aa56f3f314965e960e571c095126f455cddc
windsays <chinaxin1993@gmail.com>
2014-12-08 18:05:18 +0800

    new file:   404.php 	new file:   WordPressThemeSilence 	new file:   archives.php

---
+++ commentform-back.php
@@ -0,0 +1,49 @@
+
+<div id="respond_box">
+	<div id="respond">
+		<h3>添加评论</h3>	
+		<div class="cancel-comment-reply">
+			<small><?php cancel_comment_reply_link('取消'); ?></small>
+		</div>
+		<?php if ( get_option('comment_registration') && !$user_ID ) : ?>
+		<div class="comment-tip"><p>您必须<a href="<?php echo wp_login_url(); ?>">登录</a>才能评论</p></div>
+    <?php else : ?>
+    <form action="" method="post" id="commentform">
+      <?php if ( $user_ID ) : ?>
+	  <?php if ( is_user_logged_in() ) : ?>
+      <div class="comment-tip"><p>
+	  <?php {
+$user = wp_get_current_user();
+$link = $user->display_name.'  <a href="' . get_option('siteurl') . '/wp-login.php?action=logout">登出 &raquo;</a>';
+echo apply_filters('loginout', $link);
+}
+?>
+	  </p></div>
+	  <?php endif; ?>
+	<?php endif; ?>
+	<?php if ( ! $user_ID ): ?>
+	<div id="comment-author-info">
+
+		<p>
+			<input type="text" name="author" id="author" class="commenttext" value="<?php echo $comment_author; ?>" size="22" tabindex="1" placeholder="姓名（必填）" required />
+
+		</p>
+		<p>
+			<input type="text" name="email" id="email" class="commenttext" value="<?php echo $comment_author_email; ?>" size="22" tabindex="2" placeholder="邮件（必填）" required />
+		</p>
+		<p>
+			<input type="text" name="url" id="url" class="commenttext" value="<?php echo $comment_author_url; ?>" size="22" tabindex="3" placeholder="网址" />
+		</p>
+	</div>
+      <?php endif; ?>
+
+		<div><textarea name="comment" id="comment" tabindex="4" cols="50" rows="8"></textarea></div>
+		<div><input class="submit" name="submit" type="submit" id="submit" tabindex="5" value="发  布" /><?php comment_id_fields(); ?>
+</div>
+		
+		<?php do_action('comment_form', $post->ID); ?>
+    </form>
+	<div class="clear"></div>
+    <?php endif; ?>
+  </div> 
+  </div> 
---
+++ header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html >
+<html <?php language_attributes(); ?>>
+<head>
+<!-- Custom Title Setup -->
+<title><?php if (is_home()||is_search()) { bloginfo('name'); } else { wp_title(''); } ?></title>
+<!-- Meta -->
+<meta name="description" content="<?php echo get_option('mytheme_description'); ?>" />
+<meta content="<?php bloginfo('name'); ?>"/>
+<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<!-- Syndication -->
+<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php echo get_bloginfo_rss('rss2_url'); ?>" />
+<link rel="shortcut icon" href="<?php echo esc_url( home_url( '/' ) ); ?>favicon.ico" >
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
+<link rel="alternate" type="application/atom+xml" title="Atom 1.0" href="<?php bloginfo('atom_url'); ?>" />
+<!-- Stylesheets -->
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" /> 
+<!-- Script -->
+<!--[if lt IE 9]>
+<script src="<?php bloginfo('stylesheet_directory') ?>/js/selectivizr-min.js"></script>
+<![endif]-->
+<!-- WP Headers -->
+<?php wp_head(); ?>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory') ?>/css/audio.css" type="text/css" media="screen" /> 
+<style>
+#banner {
+	background-color: <?php echo stripslashes(get_option('mytheme_bannercolor')); ?>;
+}
+<?php echo stripslashes(get_option('mytheme_customstyle')); ?>
+</style>
+</head>
+<body id="windsays">
+	<div id="top">
+        		<?php wp_nav_menu( array('theme_location'  => 'top','container_class' => 'menu-nav-container','menu_class' => 'menu') ) ;?>
+	</div>
+		<i class="fa fa-search searchicon" title="搜索"></i>
+	<?php include(TEMPLATEPATH . '/searchform.php'); ?>
+	<div class="container">
+		<div id="site" class="hfeed site">
+			<div id="banner" class="site-header" role="banner">
+				<div class="blogtitle">
+					<h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" title="<?php bloginfo( 'name' ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+					<h3><?php bloginfo( 'description' ); ?></h3>
+				</div>
+			</div>
+			<div id="main" class="wrapper">
+				<div id="primary" class="site-content">
